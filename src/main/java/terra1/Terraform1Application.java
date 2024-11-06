c2f190ead83eca7b91ba72b86ad6b436acb6284c
chi-touch <gloebudav@gmail.com>
2024-11-06 13:47:16 +0100

    first commit

---
+++ src/main/java/terra1/Terraform1Application.java
@@ -0,0 +1,13 @@
+package terra1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Terraform1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Terraform1Application.class, args);
+	}
+
+}
---
+++ src/test/java/terra1/Terraform1ApplicationTests.java
@@ -0,0 +1,13 @@
+package terra1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Terraform1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
